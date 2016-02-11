27634b565b271639d3d40c2b173b37bd8205b7f5
Payden-McBee <pmcbee64@gmail.com>
2016-02-11 11:41:04 -0500

    Update motion_detect.m

--- motion_detect.m
+++ motion_detect.m
@@ -11,16 +11,57 @@ end
 
 n=(size(motion_filter) - 1)/2;
 
-I=size(images,2);
-J=size(images,3);
+I=size(images,1);%PM 2->1 ref rows
+J=size(images,2);%PM 2->1 ref cols
 
-if (smooth==ture)
+if (smooth==true) %PM ture to true
     %smooth image
     
     
 end
 
-for frame=n:size(images,1)-n
+%PM start insert
+endOfArray=size(images,3)-size(spatial_filter,2)+1;
+valsOfFrames = zeros(size(motion_filter,1),size(motion_filter,2));
+%frames = zeros(
+for frame=1:endOfArray  %PM 1->3 to reference # of images 
+    
+    motion_mask=zeros(I,J,endOfArray);%initialize mask to zeros
+    
+    %loop over each pixel
+    for i=1:I
+       for j=1:J
+           frames = images(i,j,frame:(frame+size(motion_filter,2)-1));
+           for k=1:size(motion_filter,2)
+               valsOfFrames(k) = frames(1,1,k);
+           end 
+           pixD=dot(double(motion_filter), double(valsOfFrames));
+           if pixD > thresh
+               motion_mask(i,j,frame)=1;
+           else
+               motion_mask(i,j,frame)=0;
+           end
+       end
+    end
+end
+
+%overLay mask and color moving points
+coloredImages = images;
+for frame=1:endOfArray  %PM 1->3 to reference # of images 
+    
+    %loop over each pixel
+    for i=1:I
+       for j=1:J
+           if motion_mask(i,j,frame)==1
+               coloredImages(i,j,frame+(size(motion_filter,2)-1)/2)= 0; %make balck if movement 
+           end
+       end
+    end
+end
+implay(coloredImages);
+%PM end insert
+%{
+for frame=n:size(images,3)-n  %PM 1->3 to reference # of images 
     
     motion_mask=zeros(I,J);%initialize mask to zeros
     
@@ -29,6 +70,7 @@ for frame=n:size(images,1)-n
        for j=1:J
            %calculate derivative at each pixel
            pixel_deriv=0;
+                    
            for filt=-n:n;
                pixel_deriv=pixel_deriv+motion_filter(n+1+filt)*images(frame+filt,i,j);
            end
@@ -43,8 +85,4 @@ for frame=n:size(images,1)-n
     %display mask and image
     
 end
-
-
-
-
-end
+%}
